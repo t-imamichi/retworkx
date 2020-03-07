efb588a436ac4e700c01140cb2821f3e70b3ba6a
Matthew Treinish <mtreinish@kortar.org>
2020-03-06 23:03:43 -0500

    Try ssl from source again

--- tools/install_openssl.sh
+++ tools/install_openssl.sh
@@ -8,10 +8,12 @@ fi
 
 wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
 tar xzvf openssl-1.1.1d.tar.gz
+rm -rf openssl-1.1.1d.tar.gz
 pushd openssl-1.1.1d
 ./config
 make
 make install
 popd
+rm -rf openssl-1.1.1d
 
 touch /tmp/openssl.installed
