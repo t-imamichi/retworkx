6375390a26de926ca1f7d611162447d27f9c8cd1
Matthew Treinish <mtreinish@kortar.org>
2020-03-06 22:40:50 -0500

    Try setting path

--- tools/install_openssl.sh
+++ tools/install_openssl.sh
@@ -9,7 +9,7 @@ fi
 wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
 tar xzvf openssl-1.1.1d.tar.gz
 pushd openssl-1.1.1d
-./config
+./config --prefix=/usr --openssldir=/usr
 make
 make install
 popd
