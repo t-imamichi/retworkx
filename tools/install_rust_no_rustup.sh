549af955bb8a14ea6b0ec3df0763fe3f6b30ef31
Matthew Treinish <mtreinish@kortar.org>
2020-03-06 22:52:58 -0500

    Cleanup sooner

--- tools/install_rust_no_rustup.sh
+++ tools/install_rust_no_rustup.sh
@@ -2,9 +2,9 @@ if [ ! -d rust-installer ]; then
     mkdir rust-installer
     wget https://static.rust-lang.org/dist/rust-nightly-s390x-unknown-linux-gnu.tar.gz
     tar xzvf rust-nightly-s390x-unknown-linux-gnu.tar.gz
+    rm -rf rust-nightly-s390x-unknown-linux-gnu.tar.gz
     pushd rust-nightly-s390x-unknown-linux-gnu
     ./install.sh
     popd
     rm -rf rust-nightly-s390x-unknown-linux-gnu
-    rm -rf rust-nightly-s390x-unknown-linux-gnu.tar.gz
 fi
